763feddc8d4e4a741cf5d13a7fb876e7d5462ee6
Tomislav Dukez <tomdu3@ymail.com>
2024-07-09 19:01:39 +0200

    add basic express app and database connection

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+
+async function testConnection() {
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+// Call the function to test the database connection
+testConnection();
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
---
+++ backend/config/config.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const sslConfig = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+};
+
+module.exports = {
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: sslConfig,
+    }
+  },
+  test: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    dialectOptions: sslConfig,
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    dialectOptions: sslConfig,
+  }
+};

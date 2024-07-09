4603aaecbe45eece638c8f577cd4e3ea877f4fa5
Tomislav Dukez <tomdu3@ymail.com>
2024-07-09 19:19:48 +0200

    refactor app to connect with db and sync models

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,17 @@
 const express = require('express');
 const app = express();
+
 const db = require('./models');
 
-async function testConnection() {
-  try {
-    await db.sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
+const PORT = process.env.PORT || 3000;
 
-// Call the function to test the database connection
-testConnection();
-
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Sync database
+db.sequelize.sync().then((req) => {
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
+
 

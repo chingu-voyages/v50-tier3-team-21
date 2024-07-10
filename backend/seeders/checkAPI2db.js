931a410cc7ec689146584eb53926ca5483e70206
Tomislav Dukez <tomdu3@ymail.com>
2024-07-10 22:48:59 +0200

    update check api and db data code

--- backend/seeders/checkAPI2db.js
+++ backend/seeders/checkAPI2db.js
@@ -1,3 +1,52 @@
+// check api data
+const apiURL = "https://menus-api.vercel.app/";
+
+fetch(apiURL)
+  .then(response => response.json())
+  .then(foods => {
+    // Restaurants dictionary
+    const restaurants = {};
+
+    // Food keys
+    const foodKeys = Object.keys(foods.pagination);
+    let foodItems = [];
+    let count = 0;
+
+    foodKeys.forEach(foodKey => {
+      foods[foodKey].forEach(food => {
+        count++;
+
+        // Count restaurants
+        if (!restaurants[food.name]) {
+          restaurants[food.name] = 1;
+        } else {
+          restaurants[food.name]++;
+        }
+
+        // Add to unique food items
+        const foodItemKey = `${food.dsc}-${food.name}`;
+        if (!foodItems.includes(foodItemKey)) {
+          foodItems.push(foodItemKey);
+        }
+
+        // Uncomment the line below to print food details
+        // console.log(`${food.name} -> $${food.price} @ -->${food.country}<`);
+      });
+    });
+
+    // Results
+    const report ={
+      numberOfFoodItems: foodItems.length,
+      numberOfRestaurants: Object.keys(restaurants).length,
+      numberOfCategories:foodKeys.length,
+    }
+    console.table(report);
+  })
+  .catch(error => {
+    console.log("Error:", error);
+  });
+
+
 const db = require("../models");
 
 const report = {};
@@ -5,15 +54,17 @@ const report = {};
 db.sequelize.sync().then(() => {
   // check fooditems if there are duplicates
  const foodItemsPromise =db.FoodItem.findAll().then((foodItems) => {
+   // check if there are duplicates - only if name and restaurantId are the same
+    
     const foodItemsMap = foodItems.reduce((map, foodItem) => {
-      map[foodItem.name] = foodItem;
+      map[`${foodItem.name}-${foodItem.restaurantId}`] = foodItem;
       return map;
     }, {});
+
     const foodItemNames = Object.keys(foodItemsMap);
     const foodItemNamesSet = new Set(foodItemNames);
-
     // add number of items to report
-    report.numberOfFoodItems = foodItemNames.length;
+    report.numberOfFoodItems = foodItems.length;
 
     if (foodItemNames.length !== foodItemNamesSet.size) {
       console.log("There are duplicates in food items.");
@@ -42,11 +93,7 @@ db.sequelize.sync().then(() => {
 
     // check categories if there are duplicates
     const categoriesPromise = db.Category.findAll().then((categories) => {
-      const categoriesMap = categories.reduce((map, category) => {
-        map[category.name] = category;
-        return map;
-      }, {});
-      const categoryNames = Object.keys(categoriesMap);
+      const categoryNames = categories.map((category) => category.name);
       const categoryNamesSet = new Set(categoryNames);
 
       // add number of items to report

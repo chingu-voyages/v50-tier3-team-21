b356fb161059944bfd5660c4f3a0571fbb4e5dae
Tomislav Dukez <tomdu3@ymail.com>
2024-07-10 18:01:15 +0200

    update and refactor seeder for API2db

--- backend/seeders/API2db.js
+++ backend/seeders/API2db.js
@@ -19,51 +19,67 @@ fetch(apiURL)
 // insert data into the database
 const db = require('../models');
 
-function insertData(data) {
-  db.sequelize.sync({ force: true }).then(() => {
-    console.log("Database synced successfully.");
-    const categories = Object.keys(data.pagination);
-    // console.log(categories)
-
-    for (let category of categories) {
-      console.log(category);
-
-      for (let item of data[category]) {
-        // console log the values from
-        /* {
-                id: 'ribs-brisket-and-burnt-ends',
-                img: 'https://goldbelly.imgix.net/uploads/showcase_media_asset/image/79619/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1',
-                name: "Joe's KC BBQ",
-                dsc: "Joe's KC Ribs, Brisket & Burnt Ends",
-                price: 110.99,
-                rate: 4,
-                country: 'Kansas City, KS',
-                latitude: 39.08532,
-                longitude: -94.688505
-                }
-   */
-        // restaurant data
-        let restaurant = {
-          name: item.name,
-          country: item.country,
-          latitude: item.latitude,
-          longitude: item.longitude,
-        };
-
-        // fooditem data
-        let foodItem = {
-          name: item.dsc,
-          imageUrl: item.img,
-          restaurant: item.name,
-          price: item.price,
-          category: category,
-        };
-
-        console.log(restaurant);
-        console.log(foodItem);
-        break;
+async function insertData(data) {
+    try {
+      await db.sequelize.sync({ force: true });
+      console.log("Database synced successfully.");
+      const categories = Object.keys(data.pagination);
+  
+      for (let categoryName of categories) {
+        let [categoryRecord, created] = await db.Category.findOrCreate({
+          where: { name: categoryName },
+          defaults: { name: categoryName }
+        });
+  
+        for (let item of data[categoryName]) {
+          // restaurant data
+          let restaurant = {
+            name: item.name,
+            country: item.country,
+            latitude: item.latitude,
+            longitude: item.longitude,
+          };
+  
+          // fooditem data
+          let foodItem = {
+            name: item.dsc,
+            imageUrl: item.img,
+            price: item.price,
+          };
+  
+          // add restaurant to database if not there
+          let [restaurantRecord, restaurantCreated] = await db.Restaurant.findOrCreate({
+            where: { name: restaurant.name },
+            defaults: restaurant
+          });
+  
+          // add foodItem to database if not there
+          let [foodItemRecord, foodItemCreated] = await db.FoodItem.findOrCreate({
+            where: { name: foodItem.name, restaurantId: restaurantRecord.id },
+            defaults: {
+              name: foodItem.name,
+              imageUrl: foodItem.imageUrl,
+              price: foodItem.price,
+              restaurantId: restaurantRecord.id
+            }
+          });
+  
+          // create association in FoodItemCategory table
+          await db.FoodItemCategory.findOrCreate({
+            where: {
+              foodItemId: foodItemRecord.id,
+              categoryId: categoryRecord.id
+            },
+            defaults: {
+              foodItemId: foodItemRecord.id,
+              categoryId: categoryRecord.id
+            }
+          });
+  
+          console.log('Inserted:', { restaurant, category: categoryRecord, foodItem });
+        }
       }
-      break;
+    } catch (error) {
+      console.log('Error:', error);
     }
-  });
-}
+  }

e1c1fc155c7004e43603fd2be9fcf629e6b522b2
marthamwangi <martyturing@gmail.com>
2024-07-13 22:01:47 +0300

    config: add colors for the application

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#49CC76",
+        secondary: "#8C63EE",
+        dark: "#291E43",
+        danger: "#F70000",
+      },
+    },
   },
   plugins: [],
-}
-
+};

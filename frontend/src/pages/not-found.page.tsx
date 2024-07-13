963c94133d697683c4ed9dfbf2e5372c7b6b97c8
marthamwangi <martyturing@gmail.com>
2024-07-13 22:09:25 +0300

    chore: create reusable button component and use it in not found page as an example

--- frontend/src/pages/not-found.page.tsx
+++ frontend/src/pages/not-found.page.tsx
@@ -1,7 +1,21 @@
+import React from "react";
+import PrimaryButton from "../components/ui/button";
+
 export const NotFoundPage = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const handleClick = () => {
+      setIsLoading(true);
+      // Simulate some asynchronous operation
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000); // Adjust timeout as needed
+    };
   return(
       <div>
           Not found page
+          <PrimaryButton isLoading={isLoading} onClick={handleClick}>
+        {isLoading ? 'Loading...' : 'Click Me'}
+      </PrimaryButton>
       </div>
   )
 }

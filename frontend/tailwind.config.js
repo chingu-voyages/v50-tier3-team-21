054e7a8270edfd98a4f969c11ef427439262cb19
King_Samuel <samueligwe63@gmail.com>
2024-07-14 07:00:48 +0100

    Revert "Feat/sharable components"

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#49CC76",
-        secondary: "#8C63EE",
-        dark: "#291E43",
-        danger: "#F70000",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+

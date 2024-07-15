f76f8887ceba15879ef8b34b048979dc08bfc5bf
Tomislav Dukez <tomdu3@ymail.com>
2024-07-15 18:21:30 +0200

    update User model with confirmPassword verification and hash password functions

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,40 +1,69 @@
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                notEmpty: true
-            }
+  const User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "The username already exists.",
+      },
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Username cannot be empty."
         },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true,
-                len: { 
-                    args: [7, 42],
-                    msg: "The password length should be between 7 and 42 characters."
-                 },
-            }
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Password cannot be empty."
         },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                notEmpty: true
-            }
+      },
+    },
+    confirmPassword: {
+      type: DataTypes.VIRTUAL,
+      set(value) {
+        if (value === this.password) {
+          const hash = bcrypt.hashSync(value, 10);
+          this.setDataValue("password", hash);
+        } else {
+          throw new Error("The passwords don't match.");
+        }
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "The email already exists.",
+      },
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Email cannot be empty."
         },
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-    });
-    return User;
-}
+        isEmail: {
+          args: true,
+          msg: "Must be a valid email address."
+        }
+      },
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+
+  return User;
+};

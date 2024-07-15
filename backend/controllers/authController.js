f1bc8edfd51a8446b4e3bf062eedbb78bb7518d6
Tomislav Dukez <tomdu3@ymail.com>
2024-07-15 18:22:30 +0200

    update signup controller to check pass length and get confirmPassword value

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,12 +3,19 @@ const db = require('../models'); // Make sure to require your models
 const signup = async (req, res, next) => {
    const body = req.body;
    console.log(body);
+   if (body.password.length < 7) {
+    return res.status(400).json({
+       status: 'fail',
+       message: 'Password must be at least 7 characters long',
+    });
+ }
 
    try {
       const newUser = await db.User.create({
          username: body.username,
          email: body.email,
-         password: body.password
+         password: body.password,
+         confirmPassword: body.confirmPassword
       });
 
       if (!newUser) {

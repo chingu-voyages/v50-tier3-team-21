9c89f8433b0c4716b961fd89a11ecea494195da6
Tomislav Dukez <tomdu3@ymail.com>
2024-07-15 18:59:00 +0200

    update add login controller and route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const db = require('../models'); // Make sure to require your models
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const generateToken = (payload) => {
     return jwt.sign(
@@ -56,4 +57,67 @@ const signup = async (req, res, next) => {
    }
 }
 
-module.exports = { signup };
+
+const login = async (req, res, next) => {
+   const { username, email, password }  = req.body;
+
+   if (!username && !email || !password) {
+      return res.status(400).json({
+         status: 'fail',
+         message: 'Please provide username or email and password',
+      });
+   }
+
+   try {
+    const condition = username ? { username } : { email };
+      
+    const result = await db.User.findOne({
+       where: condition,
+    });
+    
+    if (!result) {
+       return res.status(401).json({
+          status: 'fail',
+          message: 'Incorrect email or username',
+       });
+    }
+
+      if (!result) {
+         return res.status(401).json({
+            status: 'fail',
+            message: 'Incorrect email or username',
+         });
+      }
+
+      const isMatch = await bcrypt.compare(password, result.password);
+
+      if (!isMatch) {
+         return res.status(401).json({
+            status: 'fail',
+            message: 'Incorrect password',
+         });
+      }
+
+      const token = generateToken({
+         id: result.id
+      });
+
+      return res.status(200).json({
+         status: 'success',
+         message: 'User logged in successfully',
+         data: {
+            token
+         },
+      });
+
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+         status: 'fail',
+         message: 'Failed to login user',
+         error: error.message,
+      });
+   }
+}
+
+module.exports = { signup, login };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,4 +3,6 @@ const authController = require('../controllers/authController');
 const router = Router();
 
 router.route('/signup').post(authController.signup);
+router.route('/login').post(authController.login);
+
 module.exports = router;

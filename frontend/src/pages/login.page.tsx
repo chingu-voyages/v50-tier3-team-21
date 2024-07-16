eebbbff2c4bbbd1fd502be0d88054e4e295dc305
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-07-16 14:40:01 +0300

    fix: fix responsive issue

--- frontend/src/pages/login.page.tsx
+++ frontend/src/pages/login.page.tsx
@@ -3,8 +3,8 @@ import {AuthFooter , AuthHeader} from "./signup.page.tsx";
 
 export const LoginPage = () => {
   return(
-      <div className="w-full flex justify-center items-center py-12">
-          <aside className="sm:w-full md:max-w-md flex flex-col gap-6 bg-white shadow-md px-6 py-12 rounded-3xl">
+      <div className="w-full h-screen md:h-full flex justify-center items-center md:px-6 py-12">
+          <aside className="w-full md:max-w-md flex flex-col gap-6 bg-white md:shadow-md px-6 py-12 md:rounded-3xl">
               <AuthHeader title="Sign In" />
               <LoginForm />
               <AuthFooter isSignUp={false} />

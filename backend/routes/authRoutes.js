f9bba75d855e86e3f379c08c12e1823bf9e22180
Tomislav Dukez <tomdu3@ymail.com>
2024-07-16 15:26:59 +0200

    update routes add logout profile refreshToken and protect

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,11 @@
-const { Router } = require('express');
-const authController = require('../controllers/authController');
-const router = Router();
+const express = require('express');
+const { signup, login, profile, logout, refreshToken, protect } = require('../controllers/authController');
+const router = express.Router();
 
-router.route('/signup').post(authController.signup);
-router.route('/login').post(authController.login);
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/profile', protect, profile);
+router.post('/logout', logout);
+router.post('/refresh-token', refreshToken);
 
 module.exports = router;

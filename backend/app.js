12238bb6fea6a01827f4df5f63bdf03abaf420f5
Tomislav Dukez <tomdu3@ymail.com>
2024-07-16 20:49:17 +0200

    add cors package and app connecting frontend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
 const db = require('./models');
 const authRouter = require('./routes/authRoutes');
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
 app.use('/api/auth', authRouter);
 
 // Sync database

61028270f78eb1eeda6c88db9d980086002c32f8
Tomislav Dukez <tomdu3@ymail.com>
2024-07-16 15:15:03 +0200

    update add cookie parser to app

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const db = require('./models');
 const authRouter = require('./routes/authRoutes');
 
 
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 
 // Sync database
-db.sequelize.sync().then((req) => {
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Hello World',
+db.sequelize.sync().then((req) => {  
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      status: 'success',
+      message: 'Hello World',
   });
 })
 

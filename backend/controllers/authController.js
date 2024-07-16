f7853d0bb0b93df3d6ddc5bc94c09a07bdf9df24
Tomislav Dukez <tomdu3@ymail.com>
2024-07-16 15:40:14 +0200

    update authController - add refreshToken controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,154 +1,188 @@
-const db = require('../models'); // Make sure to require your models
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const db = require("../models"); // Make sure to require your models
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 // generate token function from .env settings
 const generateToken = (payload) => {
-    return jwt.sign(
-        payload, process.env.JWT_SECRET,{
-            expiresIn: process.env.JWT_EXPIRES_IN
-        });
-}
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
+const refreshToken = async (req, res, next) => {
+  // get refresh token from cookies
+  const { refreshToken: oldRefreshToken } = req.cookies;
 
+  // check if there is a refresh token
+  if (!oldRefreshToken) {
+    return res.status(401).json({
+      status: "fail",
+      message: "No refresh token provided",
+    });
+  }
+
+  // verify old refresh token
+  jwt.verify(oldRefreshToken, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Invalid refresh token",
+      });
+    }
+
+    // generate new refresh token
+    const newToken = generateToken(
+      { id: decoded.id },
+      process.env.JWT_EXPIRES_IN
+    );
+    const newRefreshToken = generateToken(
+      { id: decoded.id },
+      process.env.JWT_REFRESH_EXPIRES_IN
+    );
+
+    res.cookie("token", newToken, {
+      httpOnly: true,
+      secure: true, // TODO: process.env.NODE_ENV === 'production'
+    });
+    res.cookie("refreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: true, // TODO: process.env.NODE_ENV === 'production'
+    });
+
+    res.status(200).json({
+      status: "success",
+    });
+  });
+};
 
 // signup controller
 const signup = async (req, res, next) => {
-   const body = req.body;
-   console.log(body);
+  const body = req.body;
+  console.log(body);
 
-   // password length validation
-   if (body.password.length < 7) {
+  // password length validation
+  if (body.password.length < 7) {
     return res.status(400).json({
-       status: 'fail',
-       message: 'Password must be at least 7 characters long',
+      status: "fail",
+      message: "Password must be at least 7 characters long",
     });
- }
-   // attempt to create user in db (unsuccessful if doesn't pass validations)
-   try {
-      const newUser = await db.User.create({
-         username: body.username,
-         email: body.email,
-         password: body.password,
-         confirmPassword: body.confirmPassword
-      });
-
-      const result = newUser.toJSON();
-      delete result.password;
-      delete result.deletedAt;
-
-      result.token = generateToken({
-        id: result.id
-      })
-
-      if (!result) {
-         return res.status(400).json({
-            status: 'fail',
-            message: 'Failed to create user',
-         });
-      }
-
-      return res.status(201).json({
-         status: 'success',
-         message: 'User created successfully',
-         data: result,
-      });
-   } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-         status: 'fail',
-         message: 'Failed to create user',
-         error: error.message,
-      });
-   }
-}
-
-
-const login = async (req, res, next) => {
-   const { username, email, password }  = req.body;
-
-   if (!username && !email || !password) {
-      return res.status(400).json({
-         status: 'fail',
-         message: 'Please provide username or email and password',
-      });
-   }
-
-   try {
-    const condition = username ? { username } : { email };
-      
-    const result = await db.User.findOne({
-       where: condition,
+  }
+  // attempt to create user in db (unsuccessful if doesn't pass validations)
+  try {
+    const newUser = await db.User.create({
+      username: body.username,
+      email: body.email,
+      password: body.password,
+      confirmPassword: body.confirmPassword,
     });
-    
+
+    const result = newUser.toJSON();
+    delete result.password;
+    delete result.deletedAt;
+
+    result.token = generateToken({
+      id: result.id,
+    });
+
     if (!result) {
-       return res.status(401).json({
-          status: 'fail',
-          message: 'Incorrect email or username',
-       });
+      return res.status(400).json({
+        status: "fail",
+        message: "Failed to create user",
+      });
     }
 
-      if (!result) {
-         return res.status(401).json({
-            status: 'fail',
-            message: 'Incorrect email or username',
-         });
-      }
+    return res.status(201).json({
+      status: "success",
+      message: "User created successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      status: "fail",
+      message: "Failed to create user",
+      error: error.message,
+    });
+  }
+};
 
-      const isMatch = await bcrypt.compare(password, result.password);
+const login = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-      if (!isMatch) {
-         return res.status(401).json({
-            status: 'fail',
-            message: 'Incorrect password',
-         });
-      }
+  if ((!username && !email) || !password) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Please provide username or email and password",
+    });
+  }
 
-      const token = generateToken({
-         id: result.id
+  try {
+    const condition = username ? { username } : { email };
+
+    const result = await db.User.findOne({
+      where: condition,
+    });
+
+    if (!result) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Incorrect email or username",
       });
+    }
 
-      return res.status(200).json({
-         status: 'success',
-         message: 'User logged in successfully',
-         token
+    if (!result) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Incorrect email or username",
       });
+    }
 
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-         status: 'fail',
-         message: 'Failed to login user',
-         error: error.message,
+    const isMatch = await bcrypt.compare(password, result.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Incorrect password",
       });
-   }
-}
+    }
 
-   const logout = async (req, res, next) => {
-      return res.status(200).json({
-         status: 'success',
-         message: 'User logged out successfully',
-      })
-   }
+    const token = generateToken({
+      id: result.id,
+    });
 
-   const profile = async (req, res, next) => {
-      return res.status(200).json({
-         status: 'success',
-         message: 'User profile retrieved successfully',
-      })
-   }
+    return res.status(200).json({
+      status: "success",
+      message: "User logged in successfully",
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      status: "fail",
+      message: "Failed to login user",
+      error: error.message,
+    });
+  }
+};
 
-   const refreshToken = async (req, res, next) => {
-      return res.status(200).json({
-         status: 'success',
-         message: 'Token refreshed successfully',
-      })
-   }
+const logout = async (req, res, next) => {
+  return res.status(200).json({
+    status: "success",
+    message: "User logged out successfully",
+  });
+};
 
-   const protect = async (req, res, next) => {
-      return res.status(200).json({
-         status: 'success',
-         message: 'User protected successfully',
-      })
-   }
+const profile = async (req, res, next) => {
+  return res.status(200).json({
+    status: "success",
+    message: "User profile retrieved successfully",
+  });
+};
+
+const protect = async (req, res, next) => {
+  return res.status(200).json({
+    status: "success",
+    message: "User protected successfully",
+  });
+};
 module.exports = { signup, login, profile, logout, refreshToken, protect };

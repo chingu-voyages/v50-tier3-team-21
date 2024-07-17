f55b6cb7b0be8325f814226fcdc605b9bc790746
Greg <gregoryminezzi@gmail.com>
2024-07-17 18:33:40 +0200

    integrate react-hook-form

--- frontend/src/components/profile/profile-form.tsx
+++ frontend/src/components/profile/profile-form.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PrimaryButton from "../ui/button";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+// TYPES
 interface UserType {
   username: string;
   password: string;
@@ -20,87 +22,96 @@ export const ProfileForm: React.FC<ProfileFormProps> = ({
   user,
   setUser,
 }) => {
-  const [formData, setFormData] = useState<UserType>(user);
+  const { register, handleSubmit, setValue } = useForm<UserType>();
 
-  // temp styles
-  const styles = "border border-black m-1 p-3 rounded-lg text-sm text-red my-5  active:outline-secondary focus:outline-primary";
-
-  // update formData on input changes
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-    console.log(formData);
-  };
+  // prefill form with user data, recall if user changes
+  useEffect(() => {
+    setValue("username", user.username);
+    setValue("email", user.email);
+    setValue("firstName", user.firstName);
+    setValue("lastName", user.lastName);
+    setValue("password", user.password);
+  }, [user, setValue]);
 
   // on submit, edited profile will be saved in database
-  const handleSave = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const editedUser = formData;
+  const handleSave: SubmitHandler<UserType> = data => {
+    console.log(data);
+    const editedUser = data;
 
     //make API call to PUT edited user in database...something like: editUser(userId, editedUser)
     setUser(editedUser);
     alert("Profile Changed!");
   };
+
+  // temp styles
+  const styles =
+    "border border-black m-1 p-3 rounded-lg text-sm text-red my-5  active:outline-secondary focus:outline-primary";
+
   return (
     <>
       <div className="w-full">
-        <form onSubmit={handleSave}>
+        <form onSubmit={handleSubmit(handleSave)}>
+          {" "}
+          {/* SHOUDL BE HANDLESAVE*/}
           <div className="flex flex-col">
-            <label htmlFor="username" className="text-sm">Username</label>
+            <label htmlFor="username" className="text-sm">
+              Username
+            </label>
             <input
               type="text"
-              name="username"
               id="username"
-              value={formData.username}
-              onChange={handleChange}
+              {...register("username")}
               className={`${styles} text-gray-400`}
               disabled
             />
           </div>
           <div className="flex flex-col">
-            <label htmlFor="email" className="text-sm">Email </label>
+            <label htmlFor="email" className="text-sm">
+              Email{" "}
+            </label>
             <input
               type="text"
-              name="email"
               id="email"
-              value={formData.email}
-              onChange={handleChange}
+              {...register("email")}
               className={`${styles} text-gray-400`}
               disabled
             />
           </div>
           <div className="flex flex-col">
-            <label htmlFor="firstName" className="text-sm">First Name</label>
+            <label htmlFor="firstName" className="text-sm">
+              First Name
+            </label>
             <input
+              {...register("firstName")}
               type="text"
-              name="firstName"
               id="firstName"
+              {...register("firstName")}
               placeholder="First Name"
-              value={formData.firstName}
-              onChange={handleChange}
               className={styles}
             />
           </div>
           <div className="flex flex-col">
-            <label htmlFor="lastName" className="text-sm">Last Name:</label>
+            <label htmlFor="lastName" className="text-sm">
+              Last Name:
+            </label>
             <input
+              {...register("lastName")}
               type="text"
-              name="lastName"
               id="lastName"
+              {...register("lastName")}
               placeholder="Last Name"
-              value={formData.lastName}
-              onChange={handleChange}
               className={styles}
             />
           </div>
           <div className="flex flex-col">
-            <label htmlFor="password" className="text-sm">Password:</label>
+            <label htmlFor="password" className="text-sm">
+              Password:
+            </label>
             <input
               type="password"
-              name="password"
               id="password"
+              {...register("password")}
               placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
               className={styles}
             />
           </div>
@@ -110,9 +121,11 @@ export const ProfileForm: React.FC<ProfileFormProps> = ({
               <span className="font-bold text-lg">{balance.toString()}</span>
             </p>
           </div>
-
           <div className="flex justify-between w-full">
-            <PrimaryButton type="button" className="bg-white border-primary text-primary uppercase">
+            <PrimaryButton
+              type="button"
+              className="bg-white border-primary text-primary uppercase"
+            >
               Discard Changes
             </PrimaryButton>
             <PrimaryButton type="submit">Save Changes</PrimaryButton>

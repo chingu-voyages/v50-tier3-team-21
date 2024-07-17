33b9a31a0490ca061e0fec40a465db120ffcce84
Tomislav Dukez <tomdu3@ymail.com>
2024-07-17 08:45:14 +0200

    fix restore backend files after bad pr merge

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,72 +1,40 @@
-const bcrypt = require("bcrypt");
-
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: {
-        args: true,
-        msg: "The username already exists.",
-      },
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: "Username cannot be empty."
+    const User = sequelize.define('User', {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true
+            }
         },
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: "Password cannot be empty."
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: { 
+                    args: [7, 42],
+                    msg: "The password length should be between 7 and 42 characters."
+                 },
+            }
         },
-      },
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: {
-        args: true,
-        msg: "The email already exists.",
-      },
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: "Email cannot be empty."
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true
+            }
         },
-        isEmail: {
-          args: true,
-          msg: "Must be a valid email address."
-        }
-      },
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  });
-
-  User.beforeCreate(async (user) => {
-    if (user.password) {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
-    }
-  });
-
-  User.beforeUpdate(async (user) => {
-    if (user.password) {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
-    }
-  });
-
-  return User;
-};
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+    });
+    return User;
+}

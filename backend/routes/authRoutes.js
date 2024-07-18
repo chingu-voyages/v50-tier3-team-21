fb4c1f5246cc5b1da0d6d13cb5a72a52cd4bbf37
Tomislav Dukez <tomdu3@ymail.com>
2024-07-18 19:44:05 +0200

    reafactor check and refresh token into verifyToken

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { signup,login,profile,logout,protect,refreshToken,checkToken } = require('../controllers/authController');
+const { signup,login,profile,logout,protect,verifyToken } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/profile', protect, profile);
 router.post('/logout', protect, logout);
-router.post('/refresh-token', refreshToken);
-router.get('/check-token', checkToken);
+router.get('/verify-token', verifyToken);
 
 module.exports = router;

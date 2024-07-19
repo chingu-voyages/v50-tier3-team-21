6f0ba75fec32a8a2b2d4d6c12cb9c176eeb82dc7
marthamwangi <martyturing@gmail.com>
2024-07-19 23:45:25 +0300

    refactor: make button reusable component more reusable and fix prior issues in styling

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -1,21 +1,34 @@
 
 import { ButtonHTMLAttributes, ReactNode } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import cn from "../utils/cl";
 
-
-//Todo: extends props to receive  function types ( primary, secondary)
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+const buttonVariants = cva("border-2 inline-flex items-center justify-center rounded-md hover:border-dark transition ease-in-out duration-15 shadow uppercase text-sm font-semibold leading-6 px-4 py-2 ", {
+    variants: {
+        variant: {
+            primary: "border-primary text-white bg-primary hover:bg-dark hover:text-white hover:border-dark",
+            outline: "border-primary text-primary hover:border-dark hover:text-dark",
+            danger: "border-danger bg-danger/30 text-danger hover:bg-danger hover:text-white hover:border-danger"
+        },
+    },
+    defaultVariants: {
+        variant: "primary",
+    },
+});
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
     isLoading?: boolean;
     children: ReactNode;
 }
-export default function PrimaryButton({ isLoading = false, children, ...props }: ButtonProps) {
+
+export default function PrimaryButton({ isLoading = false, children, className, variant, ...props }: ButtonProps) {
     return (
-        <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-primary hover:bg-dark hover:border-dark transition ease-in-out duration-15" {...props} disabled={isLoading}>
+        <button type="button" className={cn(buttonVariants({ variant, className }))} {...props} disabled={isLoading}>
             {
                 isLoading ? (
-                    <><svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg><span>{children}</span></>
+                    <>
+                        <span>{children}</span>
+                        <span className="icon-[eos-icons--loading] animate-spin -mr-1 ml-3 h-5 w-5"></span>
+                    </>
                 ) : (
                     children
                 )}

12b8562580ae4c07e39a6f17679ef1a13a43dd5c
Tomislav Dukez <tomdu3@ymail.com>
2024-07-21 20:54:53 +0200

    Refactor nearby restaurant controller

--- backend/controllers/nearbyRestaurantController.js
+++ backend/controllers/nearbyRestaurantController.js
@@ -4,35 +4,59 @@ const Op = require('sequelize').Op;
 module.exports = {
   async getNearbyRestaurants(req, res) {
     const { latitude, longitude } = req.body;
-  
-    if (!latitude || !longitude) {
-      return res.status(400).send({ message: 'Missing latitude or longitude' });
+
+    if (latitude === undefined || longitude === undefined) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Missing latitude or longitude',
+      });
     }
-    
+
+    if (typeof latitude !== 'number' || typeof longitude !== 'number') {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Invalid latitude or longitude',
+      });
+    }
+
     // Calculate the bounding box based on the minimal distance
-    const minDistance = 0.05; // 5km in decimal degrees
+    const minDistance = 0.045; // 5km in decimal degrees
     const minLat = latitude - minDistance;
     const maxLat = latitude + minDistance;
     const minLon = longitude - minDistance;
     const maxLon = longitude + minDistance;
-  
-    const restaurants = await db.Restaurant.findAll({
-      where: {
-        [Op.and]: [
-          { latitude: { [Op.between]: [minLat, maxLat] } },
-          { longitude: { [Op.between]: [minLon, maxLon] } },
+
+    try {
+      const restaurants = await db.Restaurant.findAll({
+        where: {
+          [Op.and]: [
+            { latitude: { [Op.between]: [minLat, maxLat] } },
+            { longitude: { [Op.between]: [minLon, maxLon] } },
+          ],
+        },
+        attributes: [
+          'id',
+          'name',
+          'country',
+          'latitude',
+          'longitude',
         ],
-      },
-      attributes: [
-        'id',
-        'name',
-        'country',
-        'latitude',
-        'longitude',
-      ],
-      raw: true,
-    });
-  
-    res.json(restaurants);
+        raw: true,
+      });
+
+      if (restaurants.length === 0) {
+        return res.status(404).json({
+          status: 'fail',
+          message: 'No nearby restaurants found',
+        });
+      }
+
+      return res.status(200).json(restaurants);
+    } catch (error) {
+      return res.status(500).json({
+        status: 'fail',
+        message: 'Internal Server Error',
+      });
+    }
   }
 }

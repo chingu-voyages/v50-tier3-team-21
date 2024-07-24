3985a8f7a7f20e12325f3ce1c2d632e598293744
Tomislav Dukez <tomdu3@ymail.com>
2024-07-24 21:05:35 +0200

    refactor password controllers

--- backend/controllers/resetPasswordController.js
+++ backend/controllers/resetPasswordController.js
@@ -1,8 +1,8 @@
-const db = require('../models');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const { generateToken } = require('../helpers/jwt');
-const { sendEmail } = require('../middlewares/mail');
+const db = require("../models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { generateToken } = require("../helpers/jwt");
+const { sendEmail } = require("../middlewares/mail");
 
 // Send password reset email with token
 const sendPasswordResetEmail = async (req, res) => {
@@ -10,83 +10,113 @@ const sendPasswordResetEmail = async (req, res) => {
   try {
     const user = await db.User.findOne({ where: { email } });
     if (!user) {
-      return res.status(404).json({ status: 'fail', message: 'User not found' });
+      return res
+        .status(404)
+        .json({ status: "fail", message: "User not found" });
     }
-    const token = generateToken({ id: user.id }, process.env.JWT_RESET_SECRET,  process.env.JWT_RESET_EXPIRES_IN);
+    const token = generateToken(
+      { id: user.id },
+      process.env.JWT_RESET_SECRET,
+      process.env.JWT_RESET_EXPIRES_IN,
+    );
 
     // send email
     const address = email;
-    const link = `http://localhost:${process.env.PORT}/api/resetpassword/reset-link/${token}`;
+    const link = `${process.env.BACKEND_URL}/api/resetpassword/reset-link/${token}`;
 
     sendEmail(address, link);
 
     // send response
     return res.status(200).json({
-      status: 'success',
-      message: 'Password reset email sent',
-      link: `${process.env.BACKEND_URL}/api/resetpassword/reset-link/${token}`
+      status: "success",
+      message: "Password reset email sent",
+      link: `${process.env.BACKEND_URL}/api/resetpassword/reset-link/${token}`,
     });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ status: 'fail', message: 'Failed to send password reset email' });
+    return res
+      .status(500)
+      .json({ status: "fail", message: "Failed to send password reset email" });
   }
 };
 
 const authorizeResetToken = async (req, res) => {
   const token = req.params.token;
   if (!token) {
-    return res.status(400).json({ status: 'fail', message: 'Token must be provided' });
+    return res
+      .status(400)
+      .json({ status: "fail", message: "Token must be provided" });
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_RESET_SECRET);
     const user = await db.User.findByPk(decoded.id);
     if (!user) {
-      return res.status(404).json({ status: 'fail', message: 'User not found' });
+      return res
+        .status(404)
+        .json({ status: "fail", message: "User not found" });
     }
-    res.cookie('token', token, { httpOnly: true, sameSite: 'strict' });
+    res.cookie("token", token, { httpOnly: true, sameSite: "strict" });
     // redirect to reset password page
-    console.log(`Page is redirected to ${process.env.FRONTEND_URL}/resetpassword`)
+    console.log(
+      `Page is redirected to ${process.env.FRONTEND_URL}/auth/change-password`,
+    );
     return res.redirect(`${process.env.FRONTEND_URL}/auth/change-password`);
   } catch (error) {
     if (error instanceof jwt.JsonWebTokenError) {
-      return res.status(400).json({ status: 'fail', message: 'Invalid or expired token' });
+      return res
+        .status(400)
+        .json({ status: "fail", message: "Invalid or expired token" });
     }
     console.error(error);
-    return res.status(500).json({ status: 'fail', message: 'Failed to authorize token' });
+    return res
+      .status(500)
+      .json({ status: "fail", message: "Failed to authorize token" });
   }
-}
+};
 // Reset password with token
 const resetPassword = async (req, res) => {
   const { password, confirmPassword } = req.body;
-  
+
   const token = req.cookies.token;
   if (!token) {
-    return res.status(400).json({ status: 'fail', message: 'Token must be provided' });
+    return res
+      .status(400)
+      .json({ status: "fail", message: "Token must be provided" });
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_RESET_SECRET);
     const user = await db.User.findByPk(decoded.id);
     if (!user) {
-      return res.status(404).json({ status: 'fail', message: 'User not found' });
+      return res
+        .status(404)
+        .json({ status: "fail", message: "User not found" });
     }
-    console.log(password, confirmPassword)
+
     if (password !== confirmPassword) {
-      return res.status(400).json({ status: 'fail', message: 'Passwords do not match' });
+      return res
+        .status(400)
+        .json({ status: "fail", message: "Passwords do not match" });
     }
-    console.log(user)
-    user.password = password
-    user.confirmPassword = confirmPassword
+
+    user.password = password;
+    user.confirmPassword = confirmPassword;
     await user.save();
 
-    // delete cookie after successful password change
-    res.clearCookie('token');
-    return res.status(200).json({ status: 'success', message: 'Password reset successfully' });
+    // delete the reset cookie after successful password change
+    res.clearCookie("token");
+    return res
+      .status(200)
+      .json({ status: "success", message: "Password reset successfully" });
   } catch (error) {
     if (error instanceof jwt.JsonWebTokenError) {
-      return res.status(400).json({ status: 'fail', message: 'Invalid or expired token' });
+      return res
+        .status(400)
+        .json({ status: "fail", message: "Invalid or expired token" });
     }
     console.error(error);
-    return res.status(500).json({ status: 'fail', message: 'Failed to reset password' });
+    return res
+      .status(500)
+      .json({ status: "fail", message: "Failed to reset password" });
   }
 };
 

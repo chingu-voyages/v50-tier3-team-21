2ba5d2c8ec00d598ef2735cf5b4a4cff57b2e15d
Tomislav Dukez <tomdu3@ymail.com>
2024-07-26 18:50:08 +0200

    fix add port for fronted

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,22 @@ app.use(express.json());
 
 // Middleware
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
+// app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
+// Allow specific origin
+
+const allowedOrigins = [`${process.env.FRONTEND_URL}:5173`];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 // Routes
 app.use('/api/auth', authRouter);

4d07f12ad1cb8dd94b73e9c8fcbdc3934516f294
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-07-26 18:59:14 +0300

    fix: fix types issues from form field component that trigger error boundaries

--- frontend/src/components/authentication/form-field.tsx
+++ frontend/src/components/authentication/form-field.tsx
@@ -1,40 +1,46 @@
 import {FieldError , FieldValues , Path , UseFormRegister} from "react-hook-form";
+import {FormLabel} from "./form-label.tsx";
 
 
-export interface FormFieldProps<TFormValues> {
-    type: string;
-    placeholder: string;
-    label?: string;
-    name: Path<TFormValues>;
-    register: UseFormRegister<any>;
-    error: FieldError | undefined;
-    className?: string;
+export  interface FormFieldProps<T extends  FieldValues>{
+    type: string,
+    placeholder: string,
+    label?: string,
+    name: Path<T>,
+    register: UseFormRegister<any>
+    error: FieldError | undefined,
+    className?: string,
 }
+export const FormField = <T extends FieldValues> ({
+                                                       type,
+                                                       placeholder,
+                                                       label,
+                                                       name,
 
-export const FormField = <T extends FieldValues>({
-                                                     type,
-                                                     placeholder,
-                                                     label,
-                                                     name,
-                                                     register,
-                                                     className,
-                                                     error,
-                                                 }: FormFieldProps<T>) => {
+                                                       register,
+                                                       className,
+                                                       error
+                                                   }: FormFieldProps<T>) => {
     return (
         <div className="w-full flex flex-col gap-1">
-            {label && <label htmlFor={name as string}>{label}</label>}
+            {
+                label && <FormLabel label={label} htmlFor={name} />
+            }
             <input
                 id={name as string}
                 type={type}
                 placeholder={placeholder}
                 {...register(name)}
-                className={`${className} px-3 py-2 border-2 rounded-lg placeholder:font-light placeholder:text-[0.8em] focus:outline-0`}
+                className={`${className}  px-3 py-2 border-2 rounded-lg placeholder:font-light placeholder:text-[0.8em] focus:outline-0`}
             />
-            {error && (
-                <span className="text-danger text-sm font-medium" role="alert">
-                    {error.message}
-                </span>
-            )}
+            {
+                error &&
+                <span
+                    className="text-danger text-sm font-medium"
+                    role={"alert"}>
+                  { error?.message }
+              </span>
+            }
         </div>
-    );
-};
+    )
+}

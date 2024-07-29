a6c316bba31c4a3afc17b69288f68198815accca
marthamwangi <martyturing@gmail.com>
2024-07-29 17:50:06 +0300

    chore: refactor error messages back to a span and create a sharable toast for messages

--- frontend/src/components/authentication/error_message.tsx
+++ frontend/src/components/authentication/error_message.tsx
@@ -1,23 +1,8 @@
-import { Bounce, ToastContainer, toast } from 'react-toastify';
 
 export const ErrorMessage = ({ message }: { message: string }) => {
-  const notify = () => toast(message);
-
   return (
-    <div>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition={Bounce}
-      />
-    </div>
+    <span className="w-full bg-red-100 p-2 text-red-800 text-md rounded-lg">
+      {message}
+    </span>
   )
 }

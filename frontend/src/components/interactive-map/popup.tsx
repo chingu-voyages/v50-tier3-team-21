3dda3d2c6ee947524d5eb805a7f890ad161f7778
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-07-30 23:33:39 +0300

    feat: add function that calculate the distance between user location and the restaurant

--- frontend/src/components/interactive-map/popup.tsx
+++ frontend/src/components/interactive-map/popup.tsx
@@ -1,6 +1,8 @@
 import PrimaryButton from "../ui/button.tsx";
 import {RestaurantWithImage} from "../../services/api/interctive-map/interface.ts";
 import {useAppMapContext} from "../../provider/map.provider.tsx";
+import {useMemo} from "react";
+import {haversineDistance} from "../../utils/geospatial.ts";
 
 
 interface PopupPropsType {
@@ -8,26 +10,32 @@ interface PopupPropsType {
 }
 
 export const AppPopup = ({restaurant}: PopupPropsType) => {
-    const { handleSelectRestaurant } = useAppMapContext();
+    const { handleSelectRestaurant , geoLocation} = useAppMapContext();
+
+
+    const distance = useMemo(() => {
+        return haversineDistance([geoLocation.lat, geoLocation.long], [restaurant.latitude, restaurant.longitude])
+    },[geoLocation, restaurant])
+
    return(
-       <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[80%] sm:max-w-md md:max-w-md md:w-[336px] ">
+       <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[80%] sm:max-w-md md:max-w-md md:w-[336px] z-50 ">
            <div className="w-full bg-white flex flex-col justify-center items-start  rounded-md gap-4 z-50">
-               <div className="w-full h-[150px] overflow-hidden bg-white rounded-md">
+               <div className="w-full h-[150px] overflow-hidden bg-white rounded-md z-50">
                    <img
                        src={restaurant.imageUrl}
                        alt={`${restaurant.name} image`}
                        className="w-full h-full object-cover rounded-tl-md rounded-tr-md"
                    />
                </div>
-               <div className="flex flex-col w-full px-3  gap-2">
+               <div className="flex flex-col w-full px-3  gap-2 z-50">
                    <h3 className="font-light text-lg text-dark">{ restaurant.name }</h3>
                    <div className="flex items-center gap-2">
                        <span className="icon-[solar--map-point-wave-bold-duotone]"/>
                        <p className="font-light">{ restaurant.country }</p>
                    </div>
                    <div className="flex items-center justify-start gap-2">
-                       <span className="icon-[lets-icons:flag-duotone]"/>
-                       <span className="text-md font-light"> <strong className="text-primary font-light">5km </strong> from your location</span>
+                       <span className="icon-[lets--icons:flag-duotone]"/>
+                       <span className="text-md font-light"> <strong className="text-primary font-light">{Math.round(distance)}km</strong> from your location</span>
                    </div>
                    <div className="flex justify-start items-center gap-2 py-4">
                        <PrimaryButton variant={"outline"} onClick={() => handleSelectRestaurant(-1)}>

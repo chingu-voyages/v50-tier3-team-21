3b611a4082b91dfdeea028050025ff883886930a
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-08-05 19:54:37 +0300

    fix: add null check to prevent error

--- frontend/src/components/ui/geolocation-btn.tsx
+++ frontend/src/components/ui/geolocation-btn.tsx
@@ -9,7 +9,7 @@ export const GeolocationBtn = ({ onClick }: { onClick: (value: string) => void }
   const { data, isSuccess,isLoading  } = useReverseGeoCoding(location.long, location.lat);
 
     useEffect(() => {
-        if (isSuccess && data && data.features.length > 0) {
+        if (isSuccess && data && data.features?.length > 0) {
             const full_address = data.features[0].properties.place_formatted;
             onClick(full_address);
             setLocationEmpty();

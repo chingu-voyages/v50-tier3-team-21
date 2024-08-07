5b4a64c6dd04d8dc325a75fb2af0c78b334ebc55
Greg <gregoryminezzi@gmail.com>
2024-08-07 15:43:10 +0200

    fix floating footer on short main page

--- frontend/src/layouts/main.layout.tsx
+++ frontend/src/layouts/main.layout.tsx
@@ -3,8 +3,8 @@ import HeaderNav from "../components/ui/header";
 import FooterComponent from "../components/ui/footer"
 export const MainLayout = () => {
     return (
-        <div className="flex flex-col">
-            <HeaderNav></HeaderNav>
+        <div className="grid grid-rows-[auto_1fr_auto] min-h-[100dvh]">
+            <header><HeaderNav/></header>
             <main className="w-full mt-28">
                 <Outlet />
             </main>

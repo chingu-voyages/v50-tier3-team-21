83994506accf18d9cdf8cae0253770af8270550d
= <=>
2024-08-12 13:36:03 +0100

    updates

--- backend/models/Account.js
+++ backend/models/Account.js
@@ -2,7 +2,20 @@ module.exports = (sequelize, DataTypes) => {
   const Account = sequelize.define('Account', {
       balance: {
           type: DataTypes.DECIMAL(10, 2),
-          defaultValue: 0.00
+          defaultValue: 0.00,
+          validate: {
+            isDecimal: {
+                msg: 'Balance must be a valid decimal number.'
+            },
+            min: {
+                args: [0],
+                msg: 'Balance must be a non-negative number.'
+            },
+            max: {
+                args: [1000000],
+                msg: 'Balance cannot exceed 1,000,000.'
+            }
+        }
       },
       userId: {
         type: DataTypes.INTEGER,
